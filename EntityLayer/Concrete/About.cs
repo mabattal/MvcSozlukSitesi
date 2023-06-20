58384fff58768575eec54a3167b04e87b18278c8
Mehmet Ali Battal <mehmetalibattal@gmail.com>
2023-06-21 00:00:12 +0300

    Add project files.

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        public int AboutID { get; set; }
+        public string AbouutDetails1 { get; set; }
+        public string AbouutDetails2 { get; set; }
+        public string AboutImage1 { get; set; }
+        public string AboutImage2 { get; set; }
+    }
+}

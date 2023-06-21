888c5016d513f8d91554904e6c934fc7cd13338a
Mehmet Ali Battal <mehmetalibattal@gmail.com>
2023-06-21 21:55:55 +0300

    Abstract ve Interface bileşenleri

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        //CRUD operasyonlarını metot olarak tanımlıyoruz.
+        //Type Name();
+
+        //Listeleme
+        List<Category> List();
+
+        //Ekleme
+        void Insert(Category p);
+
+        //Güncelleme
+        void Update(Category p);
+
+        //Silme
+        void Delete(Category p);
+    }
+}

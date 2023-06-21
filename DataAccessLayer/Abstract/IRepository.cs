0c1dbd229c4ca60ddcd96297d528406d5328fba7
Mehmet Ali Battal <mehmetalibattal@gmail.com>
2023-06-22 00:20:53 +0300

    IRepository Interface ve Generic Interface

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    //Tek tek tablo adı belirtmek yerine T ile hangi entity ile işlem yapacaksak onu göndererek üzerinde işlem yapabiliriz.
+    public interface IRepository<T>
+    {
+        //herşeyi listeleme
+        List<T> List();
+        void Insert(T p);
+        void Update(T p);
+        void Delete(T p);
+
+        //şartlı listeleme(filtreleme)
+        List<T> List(Expression<Func<T, bool>> filter);
+    }
+}

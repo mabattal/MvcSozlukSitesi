746a75f9856082ff16fd9eb2064a2ce4991300a0
Mehmet Ali Battal <mehmetalibattal@gmail.com>
2023-06-21 20:30:41 +0300

    Context sınıfı eklendi

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    //Context sınıfı; yazılan DbSet türündeki propertyleri Sql'e birer tablo olarak yansıtır.
+    public class Context : DbContext
+    {
+        //About EntityLayer daki sınıfın ismi, Abouts; Sql deki tablonun ismi
+        //Pluralize kuralları gereği s takısı geliyor.
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
